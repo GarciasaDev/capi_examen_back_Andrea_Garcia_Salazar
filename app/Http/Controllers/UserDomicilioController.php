d942f7380de0324f4f03925eb571364009b653f4
Andrea Garcia <garciasa.dev@gmail.com>
2023-06-03 19:38:32 -0500

    Tablas creadas y cargadas

---
+++ app/Http/Controllers/UserDomicilioController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\user_domicilio;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Storeuser_domicilioRequest;
+use App\Http\Requests\Updateuser_domicilioRequest;
+
+class UserDomicilioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeuser_domicilioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(user_domicilio $user_domicilio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(user_domicilio $user_domicilio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateuser_domicilioRequest $request, user_domicilio $user_domicilio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(user_domicilio $user_domicilio)
+    {
+        //
+    }
+}
